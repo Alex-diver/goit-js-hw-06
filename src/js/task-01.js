7bdd5b788bca9b07fce3f209c6bff6b3eacb7796
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-05-21 17:35:16 +0300

    Manager_1

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelectorAll(`ul#categories > li.item`);
+console.log("Number of categories:", categoriesList.length);
+
+categoriesList.forEach((category) => {
+  const categoriesTitle = category.querySelector(`h2`).textContent;
+  const categoriesElements = category.querySelectorAll(`li`).length;
+
+  console.log(`Category`, categoriesTitle);
+  console.log(`Elements`, categoriesElements);
+});
